9d9d81e32a8ee3c73c29d5c901ad2de1a9c48fc6
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-06-13 00:31:15 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Components/Header';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Header></Header>
     </>
   );
 }
