5f8a04e18601707eaec5a13cdacc8e76963a463b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-06-12 19:12:40 +0600

    basic navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-500'>Hello Tailwind CSS!</h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+    </>
   );
 }
 
