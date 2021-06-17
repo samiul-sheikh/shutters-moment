e74e9cfa042321f6cfdff8efcac28f04333a98e8
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-06-18 00:31:30 +0600

    update content design

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -5,17 +5,17 @@ import sunrise from '../images/sunrise.jpg';
 const Content = () => {
     return (
         <>
-            <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+            <div className="menu-card">
                 <img src={love} alt="" className="h-full rounded mb-20 shadow" />
-                <div className="flex flex-col justify-center items-center">
+                <div className="center-content">
                     <h2 className="text-2xl mb-2">Landscape Photography</h2>
                     <p className="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, suscipit.</p>
                     <span>$50</span>
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+            <div className="menu-card">
                 <img src={sunrise} alt="" className="h-full rounded mb-20 shadow" />
-                <div className="flex flex-col justify-center items-center">
+                <div className="center-content">
                     <h2 className="text-2xl mb-2">Landscape Photography</h2>
                     <p className="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, suscipit.</p>
                     <span>$30</span>
